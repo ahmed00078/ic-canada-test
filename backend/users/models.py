96587c4302c620ff1142ba9e97fcdcb8fe309c2c
Ahmed Sidi Mohamed <ahmedsidimohammed78@gmail.com>
2024-06-12 14:26:57 +0100

    the first version on div branch

--- backend/users/models.py
+++ backend/users/models.py
@@ -7,7 +7,7 @@ class User(models.Model):
     name = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
     gender = models.CharField(max_length=255)
-    number = models.CharField(max_length=255)
+    number = models.CharField(max_length=255, default='56789045')
 
 
     def __str__(self):
