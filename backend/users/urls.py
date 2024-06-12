d55ebf8146728210bb77da1d2845336e2a5131b3
Ahmed Sidi Mohamed <ahmedsidimohammed78@gmail.com>
2024-06-12 01:11:34 +0100

    another version of the project

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,9 +1,10 @@
 from django import urls
 from django.urls import path
-from .views import UserList, UserDetail, helloWorld
+from .views import UserList, UserDetail, helloWorld, send_email_to_user
 
 urlpatterns = [
     path('users/', UserList.as_view(), name="user-list"),
     path('users/<int:pk>/', UserDetail.as_view(), name='user-detail'),
-    path('hello/', helloWorld, name='hello-world')
+    path('hello/', helloWorld, name='hello-world'),
+    path('users/send-email/<int:user_id>/', send_email_to_user, name='send-email'),
 ] 
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from rest_framework import generics
-from .models import User
 from .serializer import UserSerializer
+from django.core.mail import send_mail
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from .models import User
 
 class UserList(generics.ListCreateAPIView):
     queryset = User.objects.all()
@@ -14,3 +17,22 @@ class UserDetail(generics.RetrieveUpdateDestroyAPIView):
 
 def helloWorld(HttpRequest):
     return HttpResponse("Hello World")
+
+@api_view(['POST'])
+def send_email_to_user(request, user_id):
+    try:
+        user = User.objects.get(pk=user_id)
+        # Utilisation d'une f-string pour inclure la valeur de user_id
+        email_body = f'Bonjour, votre id est {user_id}'
+        send_mail(
+            'Cette email est pour vous',
+            email_body,
+            'ahmedsidimohammed78@gmail.com',
+            [user.email],
+            fail_silently=False,
+        )
+        return JsonResponse({'success': True, 'message': 'Email envoyé avec succès'})
+    except User.DoesNotExist:
+        return JsonResponse({'success': False, 'message': 'Utilisateur non trouvé'}, status=404)
+    except Exception as e:
+        return JsonResponse({'success': False, 'message': str(e)}, status=500)
